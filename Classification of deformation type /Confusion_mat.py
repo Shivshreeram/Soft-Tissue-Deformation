cd75636d5f102288c43b056f0249954c6f44d682
Da_lor <145596669+Shivshreeram@users.noreply.github.com>
2025-07-01 15:18:37 +0530

    Create Confusion_mat.py

---
+++ Classification of deformation type /Confusion_mat.py
@@ -0,0 +1,17 @@
+
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+y_pred = model.predict(X_train)
+y_pred_classes = np.argmax(y_pred, axis=1)
+
+conf_matrix = confusion_matrix(y_train, y_pred_classes)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
+            xticklabels=[ 'Compression', 'Rigid', 'Tension' ],
+            yticklabels=['Compression', 'Rigid', 'Tension' ])
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix of training set')
+plt.show()
